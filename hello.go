aaded817de12642bb9a8374d6178190da6358b9c
Gordon Jiang <GordonJiang1981@hotmail.com>
2015-09-30 22:47:52 +0000

    restructure the folder

--- hello.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main(){
-	fmt.Println("Hello Git hub")
-	fmt.Println("New line added")
-	fmt.Println("new feature in feature1 branch")
-}
---
+++ hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GordonJiang/stringutil"
+)
+
+
+func main(){
+	fmt.Println("Hello Git hub")
+	fmt.Println("New line added")
+	fmt.Println(stringutil.Reverse("Hello reverse world"))
+}
